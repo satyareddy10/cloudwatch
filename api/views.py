1e3046cfec4ec82f9a06affe83319124a850ddbe
satya <sattireddy1997@gmail.com>
2026-06-07 11:20:08 +0530

    added gitworkflows files

--- api/views.py
+++ api/views.py
@@ -22,7 +22,7 @@ class ItemListCreateAPIView(APIView):
         serializer = ItemSerializer(items, many=True)
         return Response({
             "success": True,
-            "message": "Items retrieved successfully",
+            "message": "kiran items retrieved successfully",
             "data": serializer.data
         }, status=status.HTTP_200_OK)
 
