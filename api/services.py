f2ad04e1b5a4e18b1e7d414eb151955d59571811
satya <sattireddy1997@gmail.com>
2026-06-07 18:02:54 +0530

    jia g

--- api/services.py
+++ api/services.py
@@ -10,8 +10,6 @@ class S3Service:
     def upload_file(file):
         s3_client = boto3.client(
             's3',
-            # aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            # aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
             region_name=settings.AWS_S3_REGION_NAME
         )
 
